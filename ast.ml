8a9291e5e46b0b725ec0e2d1d938142a6fa4ac30
Remyzorg <besognet.remy@gmail.com>
2014-03-03 16:30:53 +0100

    pretty with bindings

--- ast.ml
+++ ast.ml
@@ -22,7 +22,11 @@ let string_of_sat_var v =
   | Not i -> "!" ^ string_of_int i
   | Var i -> string_of_int i
 
-let string_of_system (_, bcnf) = 
-  String.concat "\n" @@
-    List.map (fun disj ->
-      String.concat " " (List.map string_of_sat_var disj)) bcnf
+let string_of_system (map, bcnf) = 
+  Format.sprintf "bindings {\n%s}\n%s"
+    (ICMap.fold (fun (Op (i1, i2)) v acc ->
+      acc ^ (Format.sprintf "%d %d -> %d\n" i1 i2 v)
+     ) map "")
+    (String.concat "\n" @@
+       List.map (fun disj ->
+	 String.concat " " (List.map string_of_sat_var disj)) bcnf)
