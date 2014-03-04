79628d46698a8bef1a678d814c46989f023f336f
remyzorg <besognet.remy@gmail.com>
2014-03-04 16:22:49 +0100

    merge

--- sat.mli
+++ sat.mli
@@ -1,11 +1,16 @@
 
 
 
-type result = Sat | Unsat
+exception Unsat
+exception No_literal
+
 
 type literal = Decision of Ast.sat_var | Unit of Ast.sat_var
 type model = literal list
 
 
-val solver : Ast.system -> result
+val solver : Ast.system -> model
 val translate : Ast.cnf -> Ast.system
+
+
+val string_of_model : model -> string
