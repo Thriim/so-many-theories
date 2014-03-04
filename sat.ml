3aaa6071a1a5b02c639d45fc15adc59a78ea44c2
pcouderc <pierrickcouderc@gmail.com>
2014-03-04 08:58:36 +0100

    Added .merlin + some types and functions

--- sat.ml
+++ sat.ml
@@ -22,3 +22,29 @@ let translate ast =
     env, (List.rev disj) :: accl
   ) (ICMap.empty, []) clauses in
   env, List.rev clauses
+
+type result = Sat | Unsat
+
+type literal = Decision of sat_var | Unit of sat_var
+type model = literal list
+
+(** Tests if the variables in [m] are a correct model for [c] *)
+let is_model m c =
+  (* Boolean tests by hand or using BDDs or whatever that would work! *)
+  true
+
+exception Wrong_model
+
+let satisfies m f =
+  (* If it returns false once, we don't test for the rest, just returns *)
+  try
+    List.fold_left
+      (fun acc c -> if not (is_model m c) then raise Wrong_model else acc) true f
+  with Wrong_model -> false
+
+let solver (env, bcnf) =
+  let m = [] in
+  (*
+     Core algorithm
+  *)
+  if satisfies m bcnf then Sat else Unsat
