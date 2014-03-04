f39bc78185183479a6b5a30587c35fcd7cfb5dab
pcouderc <pierrickcouderc@gmail.com>
2014-03-04 08:40:19 +0100

    Modified translate to be in the same order as the original clauses

--- sat.ml
+++ sat.ml
@@ -1,28 +1,24 @@
 
-
-
 open Ast
-
-
 
 let translate ast =
   let counter = ref 0 in
   let nvar, nclauses, clauses = ast in
-  List.fold_left (fun (env, accl) clause ->
+  let env, clauses =
+    List.fold_left (fun (env, accl) clause ->
     let env, disj = List.fold_left (fun (env, acccl) equation ->
-      let f, i1, i2 = 
+      let f, i1, i2 =
 	match equation with
 	| Neq (i1, i2) -> (fun x -> Not x), i1, i2
-	| Eq (i1, i2) -> (fun x -> Var x), i1, i2  
+	| Eq (i1, i2) -> (fun x -> Var x), i1, i2
       in
-      let var, env = try ICMap.find (Op (i1, i2)) env, env with 
+      let var, env = try ICMap.find (Op (i1, i2)) env, env with
 	  Not_found ->
 	    incr counter; !counter,
 	    ICMap.add (Op (i1, i2)) (!counter) env
       in
       env, f var :: acccl
     ) (env, []) clause  in
-    env, disj :: accl
-  ) (ICMap.empty, []) clauses
-  
-  
+    env, (List.rev disj) :: accl
+  ) (ICMap.empty, []) clauses in
+  env, List.rev clauses
