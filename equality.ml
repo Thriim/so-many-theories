39d70f59d27b708966d0236bab78e01a04d53e54
remyzorg <besognet.remy@gmail.com>
2014-03-13 10:28:04 +0100

    add propagate function in equality

--- equality.ml
+++ equality.ml
@@ -17,20 +17,68 @@ open Union_find
 type error = UnboundPropVar of int
 exception Error of error
 
-let increment_eqs h (Op (a, b)) ineqs =
+
+let increment_eqs h ineqs (Op (a, b)) =
   let ra = find h a in
   let rb = find h b in
   let h' = union h ra rb in
   if ra = rb && RelSet.mem (a, b) ineqs then None
   else Some (h', ineqs)
 
-let increment_ineqs h (Op (a, b)) ineqs =
+let increment_ineqs h ineqs (Op (a, b)) =
   let ra = find h a in
   let rb = find h b in
   if ra = rb then None
   else Some (h, RelSet.add (ra, rb) ineqs)
 
 exception End of int
+
+
+let reverse_find_env env a b =
+  List.find (fun (i, (Op (a', b'))) -> a = a' && b = b'
+  ) (IntMap.bindings env)
+
+
+let rec propagate env model (h, ineqs) =
+  let add_one a b m =
+    try
+      let i, _ = reverse_find_env env a b in
+      let lit = Decision (Var i) in
+      if (List.exists (fun lit ->
+          let var = match lit with
+            | Decision v -> v | Unit (v, _)  -> v
+          in
+          match var with
+          | Not i' -> assert false
+          | Var i' -> i' = i
+        ) m)
+      then m else lit :: m
+    with Not_found -> m
+  in
+
+  let add_model m lit =
+    let var = match lit with
+      | Decision v -> v | Unit (v, _)  -> v
+    in
+    match var with
+    | Not i -> m
+    | Var i ->
+      let Op(a, b) = begin try IntMap.find i env with
+      | Not_found -> raise (Error (UnboundPropVar i))
+      end in
+      let ra, rb = find h a, find h b in
+      if ra = rb then
+        (if ra <> a then
+            if rb <> b then
+              add_one ra b (add_one rb a m)
+            else add_one ra b m
+         else (if rb <> b then add_one rb a m else m))
+      else assert false
+
+  in
+  List.fold_left add_model model model
+
+
 
 
 module Solver = struct
@@ -44,8 +92,8 @@ module Solver = struct
 
   let add_literal env var (h, ineqs) =
     let i, f = begin match var with
-    | Var i -> i, (fun op -> increment_eqs h op ineqs)
-    | Not i -> i, (fun op -> increment_ineqs h op ineqs)
+    | Var i -> i, increment_eqs h ineqs
+    | Not i -> i, increment_ineqs h ineqs
     end in
     let op = begin try IntMap.find i env with
       Not_found -> raise (Error (UnboundPropVar i))
